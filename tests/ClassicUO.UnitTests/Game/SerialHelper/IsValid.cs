48090ef7a5d73024d0721fd48f020d6c9bfd87c7
gmocciaro <gumma1989@gmail.com>
2021-05-02 17:42:27 +0200

    fixed test

--- tests/ClassicUO.UnitTests/Game/SerialHelper/IsValid.cs
+++ tests/ClassicUO.UnitTests/Game/SerialHelper/IsValid.cs
@@ -18,7 +18,7 @@ namespace ClassicUO.UnitTests.Game.SerialHelper
 
         [Theory]
         [InlineData(0)]
-        [InlineData(0x80000000)]
+        [InlineData(4294967295)] // guglielmo: first was 0x80000000, but was bugged for AddTooltip, we added the max uint value to fix the problem
         public void IsValid_Serial_Should_Not_Be_Legal(uint serial)
         {
             ClassicUO.Game.SerialHelper.IsValid(serial)
